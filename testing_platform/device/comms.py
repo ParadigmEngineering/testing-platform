2bdacc2465f6cf16b9f1a1be51d82ce6a394584f
Ian Carroll <ianpatcarr@gmail.com>
2021-06-26 13:30:12 -0230

    Implemented the open(), close(), write(), and read(), methods for UDPServer & UDPClient classes.

--- testing_platform/device/comms.py
+++ testing_platform/device/comms.py
@@ -77,12 +77,16 @@ class UDPServer(Comms):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def open(self):
+        self.sock.bind(("127.0.0.1", 12345))
 
     def close(self):
+        self.sock.close()
 
     def write(self, data):
-    
+        self.sock.sendto(data, 12345)
+
     def read(self, num_bytes):
+        self.sock.recvfrom(num_bytes, 12345)
 
 class UDPClient(Comms):
     """ UDPClient class inherits from Comms interface class """
@@ -91,12 +95,16 @@ class UDPClient(Comms):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def open(self):
+        self.sock.bind(("127.0.0.1", 12345))
 
     def close(self):
+        self.sock.close()
 
     def write(self, data):
+        self.sock.sendto(data, 12345)
 
-    def read (self, num_bytes)
+    def read (self, num_bytes):
+        self.sock.recvfrom(num_bytes, 12345)
 
 
 # Maybe use this for functional testing of the classes in this file,
