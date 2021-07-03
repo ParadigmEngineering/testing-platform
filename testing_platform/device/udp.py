9f8fdeb855064645df5180e7b1877ed9634c4315
Ian Carroll <ianpatcarr@gmail.com>
2021-07-03 12:43:07 -0230

    Changed syntax errors in read and write methods for UDPServer and Client, added functional testing as well.

--- testing_platform/device/udp.py
+++ testing_platform/device/udp.py
@@ -14,10 +14,13 @@ class UDPServer(Comms):
         self.sock.close()
 
     def write(self, data):
-        self.sock.sendto(data, 12345)
+        self.sock.sendto(data, ("127.0.0.1", 12345))
 
     def read(self, num_bytes):
-        self.sock.recvfrom(num_bytes, 12345)
+        self.sock.recvfrom(num_bytes)
+        data,addr = self.sock.recvfrom(num_bytes)
+        print(str(data))
+        print(str(addr))
 
 class UDPClient(Comms):
     """ UDPClient class inherits from Comms interface class """
@@ -26,20 +29,39 @@ class UDPClient(Comms):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def open(self):
-        super.open()
+        super().open()
 
     def close(self):
-        super.close()
+        super().close()
 
     def write(self, data):
-        self.sock.sendto(data, 12345)
+        self.sock.sendto(data, ("127.0.0.1", 12345))
 
     def read (self, num_bytes):
-        self.sock.recvfrom(num_bytes, 12345)
+        self.sock.recvfrom(num_bytes)
+        data,addr = self.sock.recvfrom(num_bytes)
+        print(str(data))
+        print(str(addr))
 
 
 # Maybe use this for functional testing of the classes in this file,
 # not sure if this is good practice though
 if __name__ == '__main__':
+    server_sock, client_sock = socket
+
+    server = UDPServer(server_sock)
+    server.open()
+    server.write(b"Hello, I am a UDP Server!")
+
+    client = UDPClient(client_sock)
+    client.open()
+    client.write(b"Hello UDP Server, this is UDP Client")
+
+    server.read(4096)
+    client.read(4096)
+
+    server.close()
+    client.close()
+    
     pass
 
