c2ba98b08ed39be9b803497dee44017395618322
Ian Carroll <ianpatcarr@gmail.com>
2021-09-27 20:24:38 -0230

    Added configurable port & address

--- testing_platform/device/udp.py
+++ testing_platform/device/udp.py
@@ -4,28 +4,30 @@ import socket
 class UDPServer(Comms):
     """ UDPServer class inherits from Comms interface class """
 
-    def __init__(self):
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def open(self):
-        self.sock.bind(("127.0.0.1", 12345))
+        self.sock.bind((self.address, self.port))
 
     def close(self):
         self.sock.close()
 
     def write(self, data):
-        self.sock.sendto(data, ("127.0.0.1", 12345))
+        self.sock.sendto(data, (self.address, self.port))
 
     def read(self, num_bytes):
-        self.sock.recvfrom(num_bytes)
         data,addr = self.sock.recvfrom(num_bytes)
-        print(str(data))
-        print(str(addr))
+        return "Data: {dat}\nAddress: {address}".format(dat = data, address = addr)
 
 class UDPClient(Comms):
     """ UDPClient class inherits from Comms interface class """
 
-    def __init__(self):
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def open(self):
@@ -35,13 +37,11 @@ class UDPClient(Comms):
         super().close()
 
     def write(self, data):
-        self.sock.sendto(data, ("127.0.0.1", 12345))
+        self.sock.sendto(data, (self.address, self.port))
 
     def read (self, num_bytes):
-        self.sock.recvfrom(num_bytes)
         data,addr = self.sock.recvfrom(num_bytes)
-        print(str(data))
-        print(str(addr))
+        return "Data: {dat}\n Address: {address}".format(dat = data, address = addr)
 
 
 # For functional testing of the udp server and client refer to worked examples
