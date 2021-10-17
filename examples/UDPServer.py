2cff5b0cfc9891ad5933581a42b06be9172a8722
Ian Carroll <ianpatcarr@gmail.com>
2021-10-17 14:55:18 -0230

    Implemented an exit condition for UDPServer check loop.

--- examples/UDPServer.py
+++ examples/UDPServer.py
@@ -1,11 +1,17 @@
 import socket
+import keyboard
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("127.0.0.1", 12345))
 
-while True:
-    data, addr = sock.recvfrom(4096) #For sending data, use recv_from
-    print(str(data))
-    print(str(addr))
-    msg = b"Hello, I am a UDP Server!"
-    sock.sendto(msg, addr)
+try:
+    print("Press Ctrl+C to terminate the loop checking for data from UDPClient.")
+    while True:
+        data, addr = sock.recvfrom(4096) #For sending data, use recv_from
+        print(str(data))
+        print(str(addr))
+        msg = b"Hello, I am a UDP Server!"
+        sock.sendto(msg, addr)
+except KeyboardInterrupt:
+    print("\nCtrl+C terminated the loop.")
+    pass
