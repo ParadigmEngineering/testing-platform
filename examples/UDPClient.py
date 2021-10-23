db495400a5e77b08899179455ca4571b7f8ea208
Ian Carroll <ianpatcarr@gmail.com>
2021-10-23 15:14:26 -0230

    Polished UDP examples.

--- examples/UDPClient.py
+++ examples/UDPClient.py
@@ -1,10 +1,14 @@
 import socket
 
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def main():
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-msg = b"Hello UDP Server, this is UDP Client"
-client_sock.sendto(msg, ("127.0.0.1", 12345))
-data, addr = client_sock.recvfrom(4096)
-print("Server Says")
-print(str(data))
-client_sock.close()
+    msg = b"Hello UDP Server, this is UDP Client"
+    client_sock.sendto(msg, ("127.0.0.1", 12345))
+    data, addr = client_sock.recvfrom(4096)
+    print("Server Says")
+    print(str(data))
+    client_sock.close()
+
+if __name__ == '__main__':
+    main()
--- examples/UDPServer.py
+++ examples/UDPServer.py
@@ -1,17 +1,20 @@
 import socket
-import keyboard
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("127.0.0.1", 12345))
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("127.0.0.1", 12345))
 
-try:
-    print("Press Ctrl+C to terminate the loop checking for data from UDPClient.")
-    while True:
-        data, addr = sock.recvfrom(4096) #For sending data, use recv_from
-        print(str(data))
-        print(str(addr))
-        msg = b"Hello, I am a UDP Server!"
-        sock.sendto(msg, addr)
-except KeyboardInterrupt:
-    print("\nCtrl+C terminated the loop.")
-    pass
+    try:
+        print("Press Ctrl+C to terminate the loop checking for data from UDPClient.")
+        while True:
+            data, addr = sock.recvfrom(4096) #For sending data, use recv_from
+            print(str(data))
+            print(str(addr))
+            msg = b"Hello, I am a UDP Server!"
+            sock.sendto(msg, addr)
+    except KeyboardInterrupt:
+        print("\nCtrl+C terminated the loop.")
+        pass
+
+if __name__ == '__main__':
+    main()
